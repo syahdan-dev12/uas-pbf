53713481098e01430b47b9de5ec6011f937f6286
syahdan-dev12 <if22.syahdanalfarel@mhs.ubpkarawang.ac.id>
2026-06-28 01:12:12 +0700

    debug: test php runtime

--- api/index.php
+++ api/index.php
@@ -1,5 +1,3 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', '1'); // Aktifkan error hanya sementara untuk diagnosa
-
-require __DIR__ . '/../public/index.php';
+echo "PHP is working!";
+exit;
