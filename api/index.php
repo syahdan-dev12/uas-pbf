7ce4fd2c7c10175f78aa720a0a672120f75e5456
syahdan-dev12 <if22.syahdanalfarel@mhs.ubpkarawang.ac.id>
2026-06-28 00:46:11 +0700

    fix deprecated E_STRICT dengan integer 0

--- api/index.php
+++ api/index.php
@@ -1,6 +1,7 @@
 <?php
-// Gunakan nilai error_reporting yang tidak memicu deprecation untuk E_STRICT
-error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
+// Menggunakan angka integer (0) untuk mematikan semua error reporting
+// Ini menghindari penggunaan konstanta apapun yang mungkin sudah deprecated
+error_reporting(0);
 ini_set('display_errors', '0');
 
 require __DIR__ . '/../public/index.php';
