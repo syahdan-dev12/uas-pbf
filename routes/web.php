a380a6eb4b782790ebfda4127251cbb5b1fa1999
syahdan-dev12 <if22.syahdanalfarel@mhs.ubpkarawang.ac.id>
2026-06-27 23:29:51 +0700

    tambah route dokumentasi

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Tambahkan blok ini agar alamat /api/documentation bisa diakses
+Route::get('/api/documentation', function () {
+    return view('l5-swagger::index'); // Pastikan view ini tersedia dari package Anda
+});
