c3922737066a930bf60ab47dd443395ffd02ac39
syahdan-dev12 <if22.syahdanalfarel@mhs.ubpkarawang.ac.id>
2026-06-28 00:54:42 +0700

    update konfigurasi error reporting dan pembersihan cache

--- api/index.php
+++ api/index.php
@@ -1,6 +1,5 @@
 <?php
-// Menggunakan angka integer (0) untuk mematikan semua error reporting
-// Ini menghindari penggunaan konstanta apapun yang mungkin sudah deprecated
+// Gunakan angka 0 agar tidak memanggil konstanta apa pun
 error_reporting(0);
 ini_set('display_errors', '0');
 
