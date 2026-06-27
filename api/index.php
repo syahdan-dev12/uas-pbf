16ec32cdf70099b8ea765aecfe8043639a7cc085
syahdan-dev12 <if22.syahdanalfarel@mhs.ubpkarawang.ac.id>
2026-06-28 00:19:12 +0700

    bersihkan output dari pesan deprecated

--- api/index.php
+++ api/index.php
@@ -1,3 +1,6 @@
 <?php
-// Memicu Vercel untuk memuat file index.php yang berada di dalam folder public
+// Mencegah semua jenis error/warning/deprecated masuk ke output
+error_reporting(0);
+ini_set('display_errors', '0');
+
 require __DIR__ . '/../public/index.php';
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,17 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ * version="1.0.0",
+ * title="Simply Employee Management System API",
+ * description="Dokumentasi API untuk sistem manajemen karyawan"
+ * )
+ * @OA\Server(
+ * url=L5_SWAGGER_CONST_HOST,
+ * description="API Server"
+ * )
+ */
 abstract class Controller
 {
     //
